08d96d140e4c6b891da0edec7c7de3500912a3e6
weejinyoung <a8118199@gmail.com>
2022-01-08 22:51:37 +0900

    디스 실습 자바스크립트에서 this는 메소드가 포함되어 있는 객체를 가리킨다 자바스크립트의 유연성

---
+++ JavaScript_Study/about_this.js
@@ -0,0 +1,18 @@
+const o = {}
+const p = {}
+function func(){
+    switch(this){
+        case o:
+            document.write('o<br />');
+            break;
+        case p:
+            document.write('p<br />');
+            break;
+        case window:
+            document.write('window<br />');
+            break;          
+    }
+}
+func();
+func.apply(o);
+func.apply(p);
